cb8fe9baf77aae2dbcb2ceec73fbc55d70f4f680
木杉 <zhmushan@qq.com>
2020-05-23 16:33:19 +0800

    x

--- benchmarks/regexp.ts
+++ benchmarks/regexp.ts
@@ -41,20 +41,4 @@ bench({
   },
 });
 
-// bench({
-//   name: "find route",
-//   runs: 20,
-//   func(b): void {
-//     const n = new Node();
-//     for (const r of routes) {
-//       n.add(r, (): string => r);
-//     }
-//     b.start();
-//     for (let i = 0; i < 100000; ++i) {
-//       n.find("/info/zhmushan/project/router");
-//     }
-//     b.stop();
-//   },
-// });
-
 runIfMain(import.meta);
